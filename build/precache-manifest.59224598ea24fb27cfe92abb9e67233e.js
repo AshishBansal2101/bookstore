6ed47719ff876b946d9662ae9773c83d50885fca
AshishBansal2101 <bansalashish2101@gmail.com>
2022-12-16 02:18:08 +0530

    fixing package

---
+++ build/precache-manifest.59224598ea24fb27cfe92abb9e67233e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cb2828ef737c466854d0370af04ba0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "884cdcb88810de21c207",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7e03f53b16d4987e5c66",
+    "url": "/static/css/main.770f37c8.chunk.css"
+  },
+  {
+    "revision": "884cdcb88810de21c207",
+    "url": "/static/js/2.3cb6cbd9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3cb6cbd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e03f53b16d4987e5c66",
+    "url": "/static/js/main.09fffd26.chunk.js"
+  },
+  {
+    "revision": "e1fcef772f1fdaabae77",
+    "url": "/static/js/runtime-main.466e3532.js"
+  }
+]);
