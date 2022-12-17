0e3f2a1f74ea04643b89b4fb1b8be5dc1049fc8e
AshishBansal2101 <bansalashish2101@gmail.com>
2022-12-17 18:31:32 +0530

    env

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-require("dotenv").config({ path: "db/config.env" });
+// require("dotenv").config({ path: "db/config.env" });
 const fileRoute = require("./routes/file");
 require("./db/db");
 
